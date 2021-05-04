81fce133b4226f3801a1a60dbd8f57df1ff5449f
George Iliadis <george.iliadis.91@gmail.com>
2021-05-04 15:58:51 +0300

    replaced global grass animation, added hitbox and hurt box nodes

--- Effects/GrassEffect.gd
+++ Effects/GrassEffect.gd
@@ -6,3 +6,7 @@ onready var animatedSprite = $AnimatedSprite
 func _ready():
 	animatedSprite.frame=0
 	animatedSprite.play("Animate")
+
+
+func _on_AnimatedSprite_animation_finished():
+	queue_free();
--- World/Grass.gd
+++ World/Grass.gd
@@ -1,6 +1,16 @@
 extends Node2D
 
+#How to replace node with node animations 
+# Load scene
+# Get instance of new scene
+# add scene as child of node
+# set position 
 
 func _process(delta):
 	if Input.is_action_just_pressed("attack"):
+		var GrassEffect = load("res://Effects/GrassEffect.tscn")
+		var grassEffect = GrassEffect.instance()
+		var world = get_tree().current_scene
+		world.add_child(grassEffect)
+		grassEffect.global_position = global_position
 		queue_free()
